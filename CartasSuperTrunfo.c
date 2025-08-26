8339fc381bb7fc86087a667663d18ceb7babf541
gustavoriddle <gustavo.riddle@outlook.com>
2025-08-26 15:18:25 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 
 struct CartaSuperTrunfo {
    char estado;
-   char codigo[4]; // Supondo um comprimento máximo de 3 caracteres + ''
+   char codigo[4]; // Supondo um comprimento máximo de 3 caracteres''
    char cidade[50];
    int populacao;
    float area;
